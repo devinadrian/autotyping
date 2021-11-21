40a090122bbe1c172d818a791fca7714b8ce285a
depinn.rar <79341513+devinadrian@users.noreply.github.com>
2021-11-21 15:49:27 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+import discord
+from discord.ext import commands
+import colorama
+from colorama import Fore
+import asyncio
+from webserver import keep_alive
+
+import os
+
+prefix = "$$"
+
+keep_alive()
+token = os.getenv("TOKEN")
+
+bot = commands.Bot(command_prefix=prefix,
+                   help_command=None,
+                   case_insensitive=True,
+                   self_bot=True)
+
+@bot.command(pass_context=True)
+async def auto(ctx):
+	await ctx.message.delete()
+	global dmcs
+	dmcs = True
+	while dmcs:
+		async with ctx.typing():
+			print("")
+
+@bot.event
+async def on_ready():
+  print('online')
+
+bot.run(token, bot=False)
+
